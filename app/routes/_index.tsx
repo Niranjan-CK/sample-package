1c773d71b3965dc546e10fca4dd3631df085d75e
Niranjan C K <niranjan@mozilor.com>
2024-01-12 09:28:41 +0530

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import {Home} from '~/components'
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -11,6 +11,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <Home/>
       <ul>
         <li>
           <a
